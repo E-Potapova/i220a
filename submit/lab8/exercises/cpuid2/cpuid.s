0a7a680ccb58e710a25989ace7ccd23fc6ab57d0
Elizabeth Potapova <epotapo1@binghamton.edu>
2021-10-28 12:49:13 -0400

    finished lab8

--- submit/lab8/exercises/cpuid2/cpuid.s
+++ submit/lab8/exercises/cpuid2/cpuid.s
@@ -1,19 +1,20 @@
 #Currently get_cpuid(int *eax, int *ebx, int *ecx, int *edx).
-#Modify to get_cpuid(int cpuid_op, int *eax, int *ebx, int *ecx, int *edx).	
+#Modify to get_cpuid(int cpuid_op, int *eax, int *ebx, int *ecx, int *edx).
+#		      op = rdi,   eax = rsi, ebx = rdx, ecx = rcx, edx = r8
 	.text
 	.globl get_cpuid
 get_cpuid:
 	pushq   %rcx
 	pushq   %rdx
-	xorl 	%eax, %eax	#setup cpuid opcode to 0
+	movl 	%edi, %eax	#setup cpuid opcode to incoming cpuid_op
 	cpuid
 	#largest param in %eax
 	#12-char manufacturer string in ebx, edx, ecx.
-	movl	%eax, (%rdi)	#store eax cpuid result
-	movl	%ebx, (%rsi)	#store ebx cpuid result
-	popq	%rax		#pop address for edxP
-	movl    %edx, (%rax)    #store edx cpuid result
+	movl	%eax, (%rsi)	#store eax cpuid result
+	popq	%rax		#pop address for ebxP
+	movl    %ebx, (%rax)    #store ebx cpuid result
 	popq    %rax		#pop address for ecxP
 	movl	%ecx, (%rax)	#store ecx cpuid result
+	movl	%edx, (%r8)	#store edx cpuid result
 	ret
 	
